cf9a45e831e85f612822900b080af50f04335054
Nttajm <130328868+Nttajm@users.noreply.github.com>
2025-09-11 22:13:08 -0700

    Add support for new origin in CORS configuration

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express()
 app.use(express.json())
 app.use(
   cors({
-    origin: ["http://localhost:5500", "http://127.0.0.1:3000"],
+    origin: ["http://localhost:5500", "http://127.0.0.1:3000", "http://lcnjoel.com"],
     credentials: true,
     methods: ["POST"],
     allowedHeaders: ["Content-Type", "Authorization"],
